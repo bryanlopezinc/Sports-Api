fc269983e1e9b1273bd7a56a6ab549f2c41f181c
BRYAN INC <bryanlopez2kul2@yahoo.com>
2022-01-05 16:00:27 +0100

    Refactored favourites repository duplicates check

--- Modules/User/Favourites/FavouritesRepository.php
+++ Modules/User/Favourites/FavouritesRepository.php
@@ -8,7 +8,6 @@ use App\Utils\PaginationData;
 use App\ValueObjects\ResourceId;
 use Illuminate\Support\Collection;
 use Module\User\ValueObjects\UserId;
-use Illuminate\Database\QueryException;
 use Module\User\Favourites\Dto\FavouriteBuilder;
 use Module\User\Favourites\Exceptions\DuplicateEntryException;
 use Module\User\Favourites\Models\FavouriteCount;
@@ -21,40 +20,43 @@ final class FavouritesRepository
      */
     public function create(UserId $userId, int $typeId, ResourceId $favouriteId): bool
     {
-        try {
-            Model::create([
-                'user_id'       => $userId->toInt(),
-                'type_id'       => $typeId,
-                'favourite_id'  => $favouriteId->toInt()
-            ]);
-        } catch (QueryException $exception) {
-            if ($exception->getCode() === '23000') {
-                throw new DuplicateEntryException();
-            }
+        $attributes = [
+            'user_id'       => $userId->toInt(),
+            'type_id'       => $typeId,
+            'favourite_id'  => $favouriteId->toInt()
+        ];
 
-            throw $exception;
+        if (Model::query()->where($attributes)->exists()) {
+            throw new DuplicateEntryException();
         }
 
+        Model::query()->create($attributes);
+
+        $this->incrementFavouritesCount($userId);
+
+        return true;
+    }
+
+    private function incrementFavouritesCount(UserId $userId): void
+    {
         $favouritesCount = FavouriteCount::where('user_id', $userId->toInt())->first();
 
         if ($favouritesCount === null) {
             FavouriteCount::create([
-                'user_id'   => $userId->toInt(),
-                'count'     => 1
+                'user_id' => $userId->toInt(),
+                'count'   => 1
             ]);
 
-            return true;
+            return;
         }
 
         $favouritesCount->increment('count');
-
-        return true;
     }
 
     public function getFavourites(UserId $userId, PaginationData $pagination): PaginatedFavouritesCollection
     {
         /** @var \Illuminate\Pagination\Paginator */
-        $result = Model::where('user_id', $userId->toInt())
+        $result = Model::query()->where('user_id', $userId->toInt())
             ->with('type')
             ->simplePaginate($pagination->getPerPage(), page: $pagination->getPage());
 
